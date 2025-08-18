baad0cde40850b5d6424bb6f15caec11dca02e61
Kai Schmidt <kaikaliischmidt@gmail.com>
2025-08-18 13:52:14 -0700

    implement ghost ai

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 from pygame import Vector2
 from enum import Enum
-from math import floor
+from math import floor, sqrt
+from astar import find_path
+
+
+# Constants
+speed = 4
+dot_score = 10
+power_score = 50
+ghost_mul = 100
+scared_duration = 6
+eaten_speed_mul = 2
+scared_speed_mul = 0.8
 
 
 def lerp(a, b, t):
@@ -49,16 +60,25 @@ board_spec = [
 ]
 
 
+def floor_pos(pos: Vector2):
+    return Vector2(int(pos.x), int(pos.y))
+
+
 class Board:
+
     def __init__(self):
-        pass
+        self.tiles = []
+        self.time = 0
 
     def __getitem__(self, key):
-        if key.x >= self.width:
+        if key.x < 0 or key.x >= self.width:
             return None
-        if key.y >= self.height:
+        if key.y < 0 or key.y >= self.height:
             return None
         return self.tiles[int(key.y)][int(key.x)]
+
+    def scatter(self):
+        return (self.time**2 % 100) ** 1.2 < 10
 
     def wrap(self, pos):
         return Vector2(
@@ -74,11 +94,15 @@ class Board:
         key = self.wrap(key)
         self.tiles[int(key.y)][int(key.x)] = val
 
-
-speed = 3
-dot_score = 10
-power_score = 50
-ghost_mul = 100
+    def pathable_neighbors(self, start: Vector2):
+        start = floor_pos(start)
+        pathable = []
+        for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            dir = Vector2(x, y)
+            tile = self[start + dir]
+            if tile and tile != Tile.Wall:
+                pathable.append(start + dir)
+        return pathable
 
 
 class Pacman:
@@ -96,10 +120,7 @@ class Pacman:
         )
 
     def can_move_toward(self, dir, board):
-        target_idx = self.pos + dir
-        target_idx.x = floor(target_idx.x)
-        target_idx.y = floor(target_idx.y)
-        target_tile = board.get_wrapped(target_idx)
+        target_tile = board.get_wrapped(floor_pos(self.pos + dir))
         return target_tile != Tile.Wall and target_tile != Tile.Gate
 
     def update(self, dt, board):
@@ -159,16 +180,92 @@ class Pacman:
 
 class Ghost:
     class State(Enum):
-        Normal = 0
-        Fleeing = 1
-        Respawning = 2
+        ChaseScatter = 0
+        Scared = 1
+        Eaten = 2
 
-    colors = [(255, 128, 255), (255, 128, 0), (0, 255, 255), (255, 0, 0)]
+    colors = [(255, 0, 0), (0, 255, 255), (255, 128, 255), (255, 128, 0)]
 
-    def __init__(self, id, pos):
+    def __init__(self, id, pos: Vector2):
         self.id = id
-        self.pos = pos
-        self.state = Ghost.State.Normal
+        self.pos: Vector2 = pos
+        self.spawn: Vector2 = pos
+        self.dest = None
+        self.state = Ghost.State.ChaseScatter
+
+    def update(self, dt, pac: Pacman, ghosts, board: Board):
+        speed_mul = speed * (
+            eaten_speed_mul
+            if self.state == Ghost.State.Eaten
+            else scared_speed_mul if self.state == Ghost.State.Scared else 1
+        )
+        if self.dest:
+            # Move toward destination
+            diff = self.dest - self.pos
+            if diff.length() < dt * speed_mul:
+                self.pos = self.dest
+                self.dest = None
+                if (
+                    self.state == Ghost.State.Eaten
+                    and (self.pos - self.spawn).length() < 0.1
+                ):
+                    self.state = Ghost.State.ChaseScatter
+            elif diff.length() > 0:
+                self.pos = self.pos + dt * speed_mul * diff.normalize()
+        else:
+            # Choose new destination
+            start = (int(self.pos.x), int(self.pos.y))
+            scatters = dest = [
+                Vector2(board.width - 2, 1),
+                Vector2(board.width - 2, board.height - 2),
+                Vector2(1, 1),
+                Vector2(1, board.height - 2),
+            ]
+            match self.state:
+                case Ghost.State.ChaseScatter:
+                    if board.scatter():
+                        dest = scatters[self.id % 4]
+                    else:
+                        match self.id:
+                            case 0:
+                                dest = pac.pos
+                            case 1:
+                                dest = lerp(pac.pos, ghosts[0].pos, 2)
+                                if board[dest] == Tile.Wall:
+                                    dest = pac.pos
+                            case 2:
+                                for mul in [2, 1]:
+                                    if pac.can_move_toward(mul * pac.dir, board):
+                                        dest = pac.pos + mul * pac.dir
+                                        break
+                                else:
+                                    dest = pac.pos
+                            case 3:
+                                if (self.pos - pac.pos).length() <= 8:
+                                    dest = scatters[3]
+                                else:
+                                    dest = pac.pos
+                            case _:
+                                dest = self.pos
+                case Ghost.State.Scared:
+                    dest = scatters[self.id % 4]
+                case Ghost.State.Eaten:
+                    dest = self.spawn
+            dest = (int(dest.x), int(dest.y))
+            path = find_path(
+                start,
+                dest,
+                lambda pos: [
+                    (v[0], v[1])
+                    for v in board.pathable_neighbors(Vector2(pos[0], pos[1]))
+                ],
+                heuristic_cost_estimate_fnct=lambda a, b: sqrt(
+                    (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+                ),
+            )
+            path = [x for x in path] if path else []
+            if len(path) >= 2:
+                self.dest = Vector2(path[1][0], path[1][1]) + Vector2(0.5)
 
     def render(self, screen, offset, scale):
         color = [
@@ -176,12 +273,13 @@ class Ghost:
             (0, 0, 255),
             (255, 255, 255),
         ][self.state.value]
-        tl = offset + self.pos * scale + Vector2(scale * 0.1, 0)
-        size = Vector2(scale * 0.8, scale)
+        if self.state == Ghost.State.Eaten:
+            tl = offset + (self.pos - Vector2(0.5)) * scale + Vector2(0, scale * 0.4)
+            size = Vector2(scale, scale * 0.2)
+        else:
+            tl = offset + (self.pos - Vector2(0.5)) * scale + Vector2(scale * 0.1, 0)
+            size = Vector2(scale * 0.8, scale)
         pygame.draw.rect(screen, color, (tl, size))
-
-    def update(self, dt):
-        pass
 
 
 class Game:
@@ -189,8 +287,7 @@ class Game:
         self.board = Board()
         self.board.height = len(board_spec)
         self.board.width = len(board_spec[0])
-        self.board.tiles = []
-        self.ghosts = []
+        self.ghosts: list[Ghost] = []
         self.score = 0
         self.flee_timer = 0
         for j, line in enumerate(board_spec):
@@ -212,17 +309,19 @@ class Game:
                         id = "1234".index(c)
                         while len(self.ghosts) <= id:
                             self.ghosts.append(Ghost(0, Vector2()))
-                        self.ghosts[id] = Ghost(id, Vector2(i, j))
+                        self.ghosts[id] = Ghost(id, Vector2(i, j) + Vector2(0.5))
                     case "S":
                         row.append(Tile.Empty)
                         self.pac = Pacman(Vector2(i, j) + Vector2(0.5))
             self.board.tiles.append(row)
 
     def update(self, dt):
+        # Update entities
         self.pac.update(dt, self.board)
         for ghost in self.ghosts:
-            ghost.update(dt)
+            ghost.update(dt, self.pac, self.ghosts, self.board)
 
+        # Eat dots
         match self.board.get_wrapped(self.pac.pos):
             case Tile.Dot:
                 self.board.set_wrapped(self.pac.pos, Tile.Empty)
@@ -230,15 +329,28 @@ class Game:
             case Tile.Power:
                 self.board.set_wrapped(self.pac.pos, Tile.Empty)
                 self.score += power_score
-                self.flee_timer = 10
+                self.flee_timer = scared_duration
                 for ghost in self.ghosts:
-                    ghost.state = Ghost.State.Fleeing
+                    ghost.state = Ghost.State.Scared
 
+        # Pac/Ghost collision
+        for ghost in self.ghosts:
+            if (ghost.pos - self.pac.pos).length() < 0.4:
+                match ghost.state:
+                    case Ghost.State.ChaseScatter:
+                        # TODO: Death
+                        pass
+                    case Ghost.State.Scared:
+                        ghost.state = Ghost.State.Eaten
+
+        self.board.time += dt
+
+        # Flee timer
         self.flee_timer = max(0, self.flee_timer - dt)
         if self.flee_timer == 0:
             for ghost in self.ghosts:
-                if ghost.state == Ghost.State.Fleeing:
-                    ghost.state = Ghost.State.Normal
+                if ghost.state == Ghost.State.Scared:
+                    ghost.state = Ghost.State.ChaseScatter
 
     def render(self, screen):
         screen_size = Vector2(screen.get_width(), screen.get_height())
